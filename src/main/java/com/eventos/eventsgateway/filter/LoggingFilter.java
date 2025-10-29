19e1c91d366ab1adfb8294722351a4d0ca19d831
LuccaHeineck <luccacheineck@gmail.com>
2025-10-28 21:20:39 -0300

    feat: added logging for every request filtered

---
+++ src/main/java/com/eventos/eventsgateway/filter/LoggingFilter.java
@@ -0,0 +1,26 @@
+package com.eventos.eventsgateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements WebFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        logger.info("Request: {} {}", exchange.getRequest().getMethod(), exchange.getRequest().getURI());
+        exchange.getRequest().getHeaders()
+                .forEach((key, value) -> logger.info("{}: {}", key, value));
+
+        return chain.filter(exchange)
+                .doOnSuccess(aVoid -> logger.info("Response Status Code: {}", exchange.getResponse().getStatusCode()));
+    }
+}
+
