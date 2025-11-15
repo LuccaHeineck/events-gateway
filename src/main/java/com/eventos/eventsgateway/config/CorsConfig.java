fb5eaf21f7aeed5168dedf0543968e8b135b0a69
LuccaHeineck <luccacheineck@gmail.com>
2025-11-15 17:32:59 -0300

    feat: add CORS configuration to accept requests from frontend

---
+++ src/main/java/com/eventos/eventsgateway/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.eventos.eventsgateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
