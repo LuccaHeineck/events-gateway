4754448c1c9d4fe8dd3b12f1dd6524a5f1ce155b
LuccaHeineck <luccacheineck@gmail.com>
2025-11-17 23:58:28 -0300

    fix: change CORS settings

--- src/main/java/com/eventos/eventsgateway/config/CorsConfig.java
+++ src/main/java/com/eventos/eventsgateway/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002")
+                .allowedOrigins("http://177.44.248.81:3000", "http://177.44.248.81:3001", "http://177.44.248.81:3002")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
