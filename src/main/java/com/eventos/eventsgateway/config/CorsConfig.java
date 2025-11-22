2c1d169734669d54ce0ff4641c5714dde65403e2
LuccaHeineck <luccacheineck@gmail.com>
2025-11-22 15:42:28 -0300

    feat: add localhost to cors config

--- src/main/java/com/eventos/eventsgateway/config/CorsConfig.java
+++ src/main/java/com/eventos/eventsgateway/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://177.44.248.81:3000", "http://177.44.248.81:3001", "http://177.44.248.81:3002")
+                .allowedOrigins("http://177.44.248.81:3000", "http://177.44.248.81:3001", "http://177.44.248.81:3002",  "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
